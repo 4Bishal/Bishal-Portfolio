09b99af59e55fb9bb9376302b0bbfd85497abee8
Bishal Bhandari <bhandarisuraksha617@gmail.com>
2025-10-15 21:41:42 +0545

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// Exact repo name (case-sensitive) for GitHub Pages
-const repoName = "Bishal-Portfolio";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
@@ -13,6 +11,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Only use /Portfolio/ in production, localhost stays clean
-  base: mode === "production" ? `/${repoName}/` : "/",
+  base: "/"
 }));
