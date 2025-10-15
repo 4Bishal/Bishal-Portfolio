cb11887adda1302974db1e30cfb3724becace95f
Bishal Bhandari <bhandarisuraksha617@gmail.com>
2025-10-15 20:58:12 +0545

    extra type fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, ter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
 import { Home } from "./pages/Home";
 import { Toaster } from "react-hot-toast"; // ✅ Import Toaster
